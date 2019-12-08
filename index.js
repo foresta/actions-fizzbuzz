e70747686fa836276ba5323a1a82e15681c8bbcd
kz_morita <kz.morit@gmail.com>
2019-12-08 13:00:16 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+
+function fizzbuzz(number) {
+  for (let i = 0; i < number; i++) {
+    if (i % 15 == 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 == 0) {
+      console.log('Fizz');
+    } else if (i % 5 == 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+try {
+  const number = core.getInput('number');
+  console.log(`Calcurate FizzBuzz below ${number}.`);
+
+  fizzbuzz(number);
+
+  console.log(`Completed FizzBuzz!!`);
+} catch (error) {
+  core.setFailed(error.message);
+}
